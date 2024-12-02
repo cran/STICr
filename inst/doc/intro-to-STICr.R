7ee2a44d675ec4430f9eff990f102980c85ffb57
Sam Zipper <samzipper@ku.edu>
2024-12-02 15:40:24 +0000

    version 1.1

--- inst/doc/intro-to-STICr.R
+++ inst/doc/intro-to-STICr.R
@@ -41,8 +41,15 @@ head(df_calibrated)
 
 ## ----plot-calibrated-data, fig.width = 6, fig.height = 4----------------------
 # plot SpC as a timeseries and histogram
-plot(df_calibrated$datetime, df_calibrated$SpC, xlab = "Datetime", ylab = "SpC", main = "Specific Conductivity Timeseries")
-hist(df_calibrated$SpC, xlab = "Specific Conductivity", breaks = seq(0, 1025, 25), main = "Specific Conductivity Distribution")
+plot(df_calibrated$datetime, df_calibrated$SpC,
+  xlab = "Datetime", ylab = "SpC",
+  main = "Specific Conductivity Timeseries"
+)
+
+hist(df_calibrated$SpC,
+  xlab = "Specific Conductivity", breaks = seq(0, 1025, 25),
+  main = "Specific Conductivity Distribution"
+)
 
 ## ----classify-data------------------------------------------------------------
 # classify data
@@ -73,11 +80,36 @@ df_qaqc <-
   qaqc_stic_data(
     stic_data = df_classified,
     spc_neg_correction = T,
-    inspect_classification = T,
-    anomaly_size = 2,
-    window_size = 96,
-    concatenate_flags = T
+    inspect_deviation = T,
+    deviation_size = 4,
+    window_size = 96
   )
 head(df_qaqc)
 table(df_qaqc$QAQC)
+
+## ----validate-data, fig.width = 6, fig.height = 6-----------------------------
+# load and inspect sample field observation data
+head(field_obs)
+
+# use validate_stic_data to compile closest-in-time STIC reading for each field observation
+stic_validation <-
+  validate_stic_data(
+    stic_data = classified_df,
+    field_observations = field_obs,
+    max_time_diff = 30,
+    join_cols = NULL,
+    get_SpC = TRUE,
+    get_QAQC = FALSE
+  )
+
+# we can now compare the field observations and classified STIC data in the table
+head(stic_validation)
+
+# calculate percent classification accuracy
+sum(stic_validation$wetdry_obs == stic_validation$wetdry_STIC) / length(stic_validation$wetdry_STIC)
+
+# compare SpC
+plot(stic_validation$SpC_obs, stic_validation$SpC_STIC,
+  xlab = "Observed SpC", ylab = "STIC SpC"
+)
 
